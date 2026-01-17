e920721871f0d8e17e205c72feb17c35f9f1e8f8
MayMeow <3164256+MayMeow@users.noreply.github.com>
2026-01-17 20:52:47 +0100

    Refactor variable name in PostsController index
    Renamed $blogId to $currentBlog for clarity and consistency in the index method of PostsController.

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -34,13 +34,13 @@ class PostsController extends AppController
     {
         // TODO Allow pinning posts
         // Pined posts sould appear in ne menu (as a page) and not show among all posts on index page by default
-        $blogId = $this->request->getAttribute('currentBlog');
+        $currentBlog = $this->request->getAttribute('currentBlog');
 
         $query = $this->Posts->find('published')
             ->contain(['Blogs']);
 
-        if ($blogId) {
-            $query->where(['Posts.blog_id' => $blogId]);
+        if ($currentBlog) {
+            $query->where(['Posts.blog_id' => $currentBlog]);
             $query->where(['Posts.pinned' => false]);
         } else {
             $query->innerJoinWith('Blogs', function ($q) {
