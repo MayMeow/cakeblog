35d51db2972a0010ae913cd4b14ed215bb19b7e3
MayMeow <3164256+MayMeow@users.noreply.github.com>
2026-05-04 21:44:52 +0200

    aqdd styles fo frontend forms

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -1,12 +1,8 @@
 <div class="users form content">
+    <h1>🔐 Login</h1>
     <?= $this->Form->create() ?>
-    <fieldset>
-        <legend>
-            <?= __('Please enter your username and password') ?>
-        </legend>
-        <?= $this->Form->control('username') ?>
-        <?= $this->Form->control('password') ?>
-    </fieldset>
+    <?= $this->Form->control('username') ?>
+    <?= $this->Form->control('password') ?>
     <?= $this->Form->button(__('Login')); ?>
     <?= $this->Form->end() ?>
 </div>
